0c49f24ba3073a796bed32185935638e6fbaf464
Energydoggo <119084011+Energydoggo@users.noreply.github.com>
2026-05-13 10:14:38 -0400

    Update Players.js

--- Players.js
+++ Players.js
@@ -1388,7 +1388,7 @@ export const players = [
 	{ 
     id:0, 
     name:"TEST", 
-    avatar:"https://yeeplist.page.gd/images/placeholder.png", 
+    avatar:"https://github.com/Voffi-mp3/YeepList/blob/main/images/minty1.png", 
     title:"Newb Yeep", 
     score:0+1,
     rank:"https://yeeplist.page.gd/images/Newb.png",
